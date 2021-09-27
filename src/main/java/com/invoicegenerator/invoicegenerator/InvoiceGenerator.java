8d3635c450e3d81989216da7a06c52e919215f9c
shrvya <shrivyashetty@gmail.com>
2021-09-27 11:55:16 +0530

    added:Welcome to invoice generator

---
+++ src/main/java/com/invoicegenerator/invoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,14 @@
+package com.invoicegenerator.invoicegenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoiceGenerator {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to cab service");
+		InvoiceGenerator in = new InvoiceGenerator();
+
+	}
+
+}
